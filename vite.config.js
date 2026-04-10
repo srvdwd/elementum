ff243bd807d75c356b5cd5958d7b3f5be302387b
Sourabh Dwivedi <139615147+srvdwd@users.noreply.github.com>
2026-04-11 02:19:25 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    allowedHosts: ['elementum-cs3p.onrender.com']
+  },
+  // If you are seeing this during 'npm run dev', use the 'server' block instead:
+  server: {
+    allowedHosts: ['elementum-cs3p.onrender.com']
+  }
 });
